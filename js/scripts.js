c2e4deae5f65d1f11709734bcb77163b67114977
Denis <den0015@yandex.ru>
2020-11-04 01:55:55 +0300

    Предзащитная проверка
    Прошелся по всем критериям. На мой замылиный взгляд все ОК)

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,31 @@
-const contactLink = document.querySelector(".contact-us");
-const writeUsModal = document.querySelector(".modal-write-us");
+const contactLink = document.querySelector('.contact-us');
+const writeUsModal = document.querySelector('.modal-write-us');
 const writeUsForm = writeUsModal.querySelector('.write-us-form');
-const writeUsName = writeUsModal.querySelector("#your-name");
+const writeUsName = writeUsModal.querySelector('#your-name');
 const writeUsEmail = writeUsModal.querySelector('#your-email');
 const writeUsText = writeUsModal.querySelector('#message-text');
-const writeUsClose = writeUsModal.querySelector(".modal-close");
+const writeUsClose = writeUsModal.querySelector('.modal-close');
 
-const mapLink = document.querySelector(".index-map-link");
-const mapModal = document.querySelector(".modal-map");
-const mapClose = mapModal.querySelector(".modal-close");
+const mapLink = document.querySelector('.index-map-link');
+const mapModal = document.querySelector('.modal-map');
+const mapClose = mapModal.querySelector('.modal-close');
 
 let isStorageSupport = true;
-let storageName = "";
-let storageEmail = "";
+let storageName = '';
+let storageEmail = '';
 
 // Modal Contact
 
 try {
-  storageName = localStorage.getItem("name");
-  storageEmail = localStorage.getItem("email");
+  storageName = localStorage.getItem('name');
+  storageEmail = localStorage.getItem('email');
 } catch (err) {
   isStorageSupport = false;
 }
 
-contactLink.addEventListener("click", function (evt) {
+contactLink.addEventListener('click', function (evt) {
   evt.preventDefault();
-  writeUsModal.classList.add("modal-show");
-
+  writeUsModal.classList.add('modal-show');
   if (!storageName && storageEmail) {       
     writeUsEmail.value = storageEmail;
     writeUsName.focus();
@@ -37,55 +36,59 @@ contactLink.addEventListener("click", function (evt) {
   } else {
     writeUsName.focus();
   }
-  
 });
 
-writeUsForm.addEventListener("submit", function (evt) {
+writeUsForm.addEventListener('submit', function (evt) {
   if (!writeUsEmail.value) {
     evt.preventDefault();
-    writeUsEmail.classList.add("valid-error");
+    writeUsModal.classList.remove('modal-error');
+    writeUsModal.offsetWidth = writeUsModal.offsetWidth;
+    writeUsModal.classList.add('modal-error');
+    writeUsEmail.classList.add('valid-error');
     writeUsEmail.focus();
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("name", writeUsName.value);
-      localStorage.setItem("email", writeUsEmail.value); 
+      localStorage.setItem('name', writeUsName.value);
+      localStorage.setItem('email', writeUsEmail.value); 
     }
   }
 });
 
-writeUsClose.addEventListener("click", function (evt) {
+writeUsClose.addEventListener('click', function (evt) {
   evt.preventDefault();
-  writeUsModal.classList.remove("modal-show");
-  writeUsEmail.classList.remove("valid-error");
+  writeUsModal.classList.remove('modal-show');
+  writeUsModal.classList.remove('modal-error');
+  writeUsEmail.classList.remove('valid-error');
 });
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    if (writeUsModal.classList.contains("modal-show")) {
+    if (writeUsModal.classList.contains('modal-show')) {
       evt.preventDefault();
-      writeUsModal.classList.remove("modal-show");
-      writeUsEmail.classList.remove("valid-error");
+      writeUsModal.classList.remove('modal-show');
+      writeUsModal.classList.remove('modal-error');
+      writeUsEmail.classList.remove('valid-error');
     }
   }
 });
 
 // Modal Map
 
-mapLink.addEventListener("click", function (evt) {
+mapLink.addEventListener('click', function (evt) {
   evt.preventDefault();
-  mapModal.classList.add("modal-show");
+  mapModal.classList.add('modal-show');
 });
 
-mapClose.addEventListener("click", function (evt) {
+mapClose.addEventListener('click', function (evt) {
   evt.preventDefault();
-  mapModal.classList.remove("modal-show");
+  mapModal.classList.remove('modal-show');
 });
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-      if (mapModal.classList.contains("modal-show")) {
+      if (mapModal.classList.contains('modal-show')) {
         evt.preventDefault();
-        mapModal.classList.remove("modal-show");
+        mapModal.classList.remove('modal-show');
     }
   }
 });
